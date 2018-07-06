d0115cf4d5d09216528b519718fbcd51c71bcead
amarkotasky <amarkotasky@gmail.com>
2018-07-07 00:13:20 +0530

    Update zivame_start.sh

--- zivame_start.sh
+++ zivame_start.sh
@@ -12,7 +12,7 @@ echo $INT_CDN_URI
 ###################################################################################################
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # nvm use $1
 
