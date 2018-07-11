77728b8445c0cacd6457b43c0a54e7992c1c84a9
amarkotasky <amarkotasky@gmail.com>
2018-07-11 14:03:35 +0530

    Update zivame_start.sh

--- zivame_start.sh
+++ zivame_start.sh
@@ -11,30 +11,14 @@ echo ". environment.sh" >> ~/.bashrc \
 
 echo $INT_CDN_URI
 ###################################################################################################
-######                                    Set .nvm
+######                             Check Node & Npm Version
 ###################################################################################################
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm use $1
-
-if [ $? == 0 ] ;then echo "Already Installed Node $1";else echo "Installing Node $1";nvm install $1 && nvm use $1;fi
-
-# nvm use $1
 node -v
 npm --version
-
-###################################################################################################
-#####                                    Update NPM
-###################################################################################################
-npm update
-
-if [ $? == 0 ] ;then echo "Npm Updated Successfully";else echo "Npm Update Failed !!!!";fi
 
 ###################################################################################################
 ######                                Starting Node Server
 ###################################################################################################
 echo "Starting Server"
-npm install nodemon --save
-npm start
+nodemon --trace-warnings server.js
